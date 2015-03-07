7274062155b2891362a866a7606fcf4ce269a603
Ira Levin <ilevin@bdata.com>
2015-03-06 17:19:28 -0800

    Finish first round of cucumber testing

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 Given /^(?:|I )am on (.+)$/ do |page_name|
 	visit path_to(page_name)
 end
@@ -19,5 +21,14 @@ Then /^(?:|I )should see "([^"]*)"$/ do |text|
 		expect(page).to have_content(text)
 	else
 		assert page.has_content?(text)
+	end
+end
+
+And /^(?:|I )should be on (.+)$/ do |page_name|
+	current_path = URI.parse(current_url).path
+	if current_path.respond_to? :should
+		expect(current_path).to eq(path_to(page_name))
+	else
+		assert_equal path_to(page_name), current_path
 	end
 end
--- features/support/paths.rb
+++ features/support/paths.rb
@@ -1,56 +1,33 @@
-require 'byebug'
-
 module NavigationHelpers
+	# Maps a name to a path. Used by the
+	#
+	#   When /^I go to (.+)$/ do |page_name|
+	#
+	# step definition in web_steps.rb
+	#
 	def path_to(page_name)
 		case page_name
 
-			when /^the list of (.*?)$/
-				models_prose = $1
-				route = "#{model_prose_to_route_segment(models_prose)}_path"
-				send(route)
+			when /the home\s?page/
+				'/'
 
-			when /^the (page|form) for the (.*?) above$/
-				action_prose, model_prose = $1, $2
-				route = "#{action_prose == 'form' ? 'edit_' : ''}#{model_prose_to_route_segment(model_prose)}_path"
-				model = model_prose.classify.constantize
-				send(route, model.last)
+			when /the project page for "([^\"]*)"/
+				project_path(Project.find_by_name!($1))
 
-			when /^the (page|form) for the (.*?) "(.*?)"$/
-				action_prose, model_prose, identifier = $1, $2, $3
-				path_to_show_or_edit(action_prose, model_prose, identifier)
-
-			when /^the (.*?) (page|form) for "(.*?)"$/
-				model_prose, action_prose, identifier = $1, $2, $3
-				path_to_show_or_edit(action_prose, model_prose, identifier)
-
-			when /^the (.*?) form$/
-				model_prose = $1
-				route = "new_#{model_prose_to_route_segment(model_prose)}_path"
-				send(route)
-
-			when /^the home page$/
-				send("root_path")
-
-			# ....
-			# your own paths go here
-			# ...
+			# ------------------------
+			# Add more mappings here.
+			# ------------------------
 
 			else
-				byebug
-
+				begin
+					page_name =~ /the (.*) page/
+					path_components = $1.split(/\s+/)
+					self.send(path_components.push('path').join('_').to_sym)
+				rescue Object => e
+					raise "Can't find mapping from \"#{page_name}\" to a path.\n" +
+							      "Now, go and add a mapping in #{__FILE__}"
+				end
 		end
-
-	end
-
-	private
-
-	def path_to_show_or_edit(action_prose, model_prose, identifier)
-		model = model_prose.classify.constantize
-		send(route, model.find_by_anything!(identifier))
-	end
-
-	def model_prose_to_route_segment(model_prose)
-		model_prose.gsub(/[\ \/]/, '_')
 	end
 end
 
