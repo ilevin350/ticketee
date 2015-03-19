91faebae9392a6e7b3bdc0b0fae8d36d9177fba0
Ira Levin <ilevin@bdata.com>
2015-03-18 20:10:20 -0700

    Start working on deleting projects

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -24,6 +24,14 @@ Then /^(?:|I )should see "([^"]*)"$/ do |text|
 	end
 end
 
+Then /^(?:|I )should not see "([^"]*)"$/ do |text|
+	if page.respond_to? :should
+		expect(page).to have_no_content(text)
+	else
+		assert page.has_no_content?(text)
+	end
+end
+
 And /^(?:|I )should be on (.+)$/ do |page_name|
 	current_path = URI.parse(current_url).path
 	if current_path.respond_to? :should
