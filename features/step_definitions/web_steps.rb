a4cb52a02ea2318c5cd3a0733d3dde1802fef959
Ira Levin <ilevin@bdata.com>
2015-03-31 05:33:41 -0700

    Section 5.2

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -1,5 +1,12 @@
 require 'byebug'
 
+module WithinHelpers
+	def with_scope(locator)
+		locator ? within(*selector_for(locator)) { yield } : yield
+	end
+end
+World(WithinHelpers)
+
 Given /^(?:|I )am on (.+)$/ do |page_name|
 	visit path_to(page_name)
 end
